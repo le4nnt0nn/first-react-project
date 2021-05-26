c2f442c4ab8b3f99f898a1f66354931122ba09dc
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-27 00:53:01 +0200

    app.js : added TodoList component

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoList from "./components/todoList";
 
 function App() {
   return (
     <div className="App">
         <h1>To-Do List</h1>
+        <div className="todo-list">
+          <TodoList />
+        </div>
+        
     </div>
   );
 }
