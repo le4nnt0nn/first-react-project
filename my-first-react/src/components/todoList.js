770411b41d2e37006844063e77754aff85189f5a
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-27 00:49:55 +0200

    components todoList.js : create props

--- my-first-react/src/components/todoList.js
+++ my-first-react/src/components/todoList.js
@@ -2,10 +2,12 @@ import React from "react";
 
 const todos = ["Terminar Trabajo", "Estudiar PHP", "Aprender React", "Salir al cine"];
 
+const Todo = props => <li>{props.todo}</li>;
+
 const TodoList = () => (
   <ul>
-    {todos.map(task => (
-      <li key={task}>{task}</li>
+    {todos.map(todo => (
+      <Todo todo={todo} key={todo} />
     ))}
   </ul>
 );
