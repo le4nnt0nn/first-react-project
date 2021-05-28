bd7cd574c6b63c068aabfc61fee6cdfaf286f7b3
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-28 13:35:54 +0200

    app js : removed constructor

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -3,23 +3,14 @@ import TodoList from "./components/todoList";
 import TodoForm from "./components/todoForm";
 
 function App() {
-
-  /* constructor */
-  constructor(props) {
-    super(props);
-    this.state = {
-      todos: ["Finish homework", "Wash dishes", "Clean room", "Make waffles"],
-    };
-  }
-
   return (
     <div className="App">
         <h1>To-Do List</h1>
         <div className="todo-list">
-          <TodoList todos={this.state.todos} />
+          <TodoList />
           <TodoForm />
         </div>
-        
+
     </div>
   );
 }
