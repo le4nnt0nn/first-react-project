ae9ff06293244865fc9ad96fa587b52e935628eb
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-28 13:23:44 +0200

    app js : added todos state to TodoList

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
         <h1>To-Do List</h1>
         <div className="todo-list">
-          <TodoList />
+          <TodoList todos={this.state.todos} />
           <TodoForm />
         </div>
         
