c6bdb3c184475f810ab0e05db6abace7ae19a475
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-05-29 12:57:15 +0200

    components todoList : added props.todos

--- my-first-react/src/components/todoList.js
+++ my-first-react/src/components/todoList.js
@@ -6,10 +6,11 @@ const Todo = props => <li>{props.todo}</li>;
 
 const TodoList = () => (
   <ul>
-    {todos.map(todo => (
+    {props.todos.map(todo => (
       <Todo todo={todo} key={todo} />
     ))}
   </ul>
+
 );
 
 export default TodoList;
