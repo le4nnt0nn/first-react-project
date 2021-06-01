c8bbdf0f5b5fdd5d14f519c45f91b7c086109dee
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-01 13:34:01 +0200

    components todoList : added style for list

--- my-first-react/src/components/todoList.js
+++ my-first-react/src/components/todoList.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const todos = ["Aprender React", "Practicar Visual Basic", "Salir con amigos", "Ver serie de anime"];
 
-const Todo = props => <li>{props.todo}</li>;
+const Todo = props => <li style={{paddingTop: "20px"}}>{props.todo}</li>;
 
 const TodoList = props => (
-  <ul>
+  <ul style={{marginTop: "-10px", marginBottom:"50px", listStyle: "none", textAlign: "center", justifyContent: "center"}}>
     {props.todos.map(todo => (
       <Todo todo={todo} key={todo} />
     ))}
