42fb725db74e7359bfb9089c10d07c626079b5b1
Antonio <71550295+le4nnt0nn@users.noreply.github.com>
2021-06-03 11:44:44 +0200

    components todoList : added margin auto and padding initial to ul

--- my-first-react/src/components/todoList.js
+++ my-first-react/src/components/todoList.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const todos = ["Aprender React", "Practicar Visual Basic", "Salir con amigos", "Ver serie de anime"];
 
-const Todo = props => <li onClick={() => props.deleteTodo(props.todo)} style={{paddingTop: "20px", cursor:"pointer"}}>{props.todo}</li>;
+const Todo = props => <li onClick={() => props.deleteTodo(props.todo)} style={{padding: "10px 0 10px", cursor:"pointer"}}>{props.todo}</li>;
 
 const TodoList = props => (
-  <ul style={{marginTop: "-10px", marginBottom:"50px", listStyle: "none", textAlign: "center", justifyContent: "center"}}>
+  <ul style={{margin: "auto", listStyle: "none", textAlign: "center", justifyContent: "center",  padding: "initial"}}>
     {props.todos.map(todo => (
       <Todo todo={todo} 
       key={todo} 
